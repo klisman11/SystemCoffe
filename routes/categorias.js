a98e218c6c3bb5300fae827c303fd9a54b68c7b4
[klisman28] <klisoved28@gmail.com>
2022-09-30 22:50:55 -0600

    url basicas

--- routes/categorias.js
+++ routes/categorias.js
@@ -6,9 +6,35 @@ const { validarcampos } = require('../middlewares/ValidarCampos');
 
 const router = Router();
 
+//Obtener todas las  categorias publlioo
 router.get('/', (req, res) => {
+    res.json('Obetener')
+});
 
+
+//Obtener todas las  categorias publlio por id 
+router.get('/:id', (req, res) => {
+    res.json('Obetener por id ')
 
 });
+
+//Crear categoria privado
+router.post('/', (req, res) => {
+    res.json('Agregar categoria')
+
+});
+
+
+//Actualizar categoria 
+router.put('/:id', (req, res) => {
+    res.json('Actualizar categoria ')
+
+});
+//Borrar categoria Administrador 
+router.delete('/:id', (req, res) => {
+    res.json('Borrar categoria')
+
+});
+
 
 module.exports = router;
